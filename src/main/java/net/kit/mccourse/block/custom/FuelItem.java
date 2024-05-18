3296d96c322d7314cf3db28641933b5e8c0dae5b
Topster99 <61192450+Topster99@users.noreply.github.com>
2024-05-17 18:50:59 -0700

    Add files via upload

---
+++ src/main/java/net/kit/mccourse/block/custom/FuelItem.java
@@ -0,0 +1,23 @@
+package net.kit.mccourse.block.custom;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.jetbrains.annotations.Nullable;
+
+public class FuelItem extends Item {
+    private int burnTime = 0;
+
+
+    public FuelItem(Properties pProperties, int burnTime) {
+        super(pProperties);
+        this.burnTime = burnTime;
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+        return this.burnTime;
+    }
+}
+
+//ALL fuel items only need this one class to exist, just add more fuel items to the ModItems class
